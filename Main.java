53369b40cce673c6fd0506b94d7e13e86dc76344
Liana <wong3327@mydusd.org>
2025-05-10 22:17:25 -0700

    add petNum, getName and setName

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Pet dog1 = new Pet("Prometheus");
-        Pet cat1 = new Pet("SUPER FENT WARRIOR");
-        System.out.println(dog1);
-        System.out.println(cat1);
+        Pet pet1 = new Pet();
+        System.out.println(pet1.getName());
+        pet1.setName("Prometheus");
+        System.out.println(pet1);
+
+        Pet pet2 = new Pet();
+        pet2.setName("SUPER FENT WARRIOR");
+        System.out.println(pet2);
     }
 }
--- Pet.java
+++ Pet.java
@@ -6,7 +6,7 @@ public class Pet {
 
     //Private instance variables - general info (will not be changed)
     private String species;
-    private int variation; //different colors/appearances per species
+    private int variation; //different colors/appearances per species (3 types)
     private Occupation occupation;
     private String img;
 
@@ -15,10 +15,14 @@ public class Pet {
     private int level; //upgrade over time -> demo will upgrade manually
     private int hunger; 
 
-    public Pet(String n) {
-        name = n;
+    private static int petNum = 0; //keeps track of total number of ppets
+
+    public Pet() {
+        petNum++;
+
+        name = "Pet #" + petNum;
         species = speciesList[(int)(Math.random()*speciesList.length)];
-        variation = (int)(Math.random()*2) +1;
+        variation = (int)(Math.random()*3) +1;
         occupation = new Occupation();
         img = "img/pet/" + species + variation + ".jpg";
 
@@ -30,6 +34,10 @@ public class Pet {
     public int getVariation() {return variation;}
     public String getOccuptation() {return occupation.getJobTitle();}
     public String getImage() {return img;}
+    
+    
+    public String getName() {return name;}
+    public void setName(String n) {name = n;}
 
     @Override 
     public String toString() {
